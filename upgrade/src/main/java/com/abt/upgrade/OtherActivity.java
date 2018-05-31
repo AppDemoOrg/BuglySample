5de9e062566d7c2ad5f237b84eadc732510ebaae
hwq <weiqi.huang@pisofttech.com>
2018-05-31 18:14:57 +0800

    添加bugly的upgrade

---
+++ upgrade/src/main/java/com/abt/upgrade/OtherActivity.java
@@ -0,0 +1,19 @@
+package com.abt.upgrade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @描述：     @OtherActivity
+ * @作者：     @黄卫旗
+ * @创建时间： @21/05/2018
+ */
+public class OtherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+
+}
